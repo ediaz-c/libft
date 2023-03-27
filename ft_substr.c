1da9333a399a3f685bb5880604e3c651501e3d1a
Erick Diaz <ediaz--c@c1r3s6.42madrid.com>
2023-03-27 14:41:25 +0200

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,19 +6,19 @@
 /*   By: ediaz--c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 12:34:05 by ediaz--c          #+#    #+#             */
-/*   Updated: 2023/03/21 17:06:56 by ediaz--c         ###   ########.fr       */
+/*   Updated: 2023/03/24 16:49:31 by ediaz--c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_len(char *str, size_t len)
+unsigned char	ft_len(char *str, unsigned int start, size_t len)
 {
-	size_t	size;
+	unsigned char	size;
 
-	size = len;
-	if (ft_strlen(str) < len)
-		size = ft_strlen(str);
+	size = len - start;
+	if (ft_strlen(str) <= len)
+		size = ft_strlen(str) - start;
 	return (size);
 }
 
@@ -33,19 +33,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	i = 0;
 	if (!s)
 		return (0);
-	size = ft_len((char *)s, len);
+	size = ft_len((char *)s, start, len);
 	str = (char *)malloc (sizeof(*s) * (size + 1));
 	if (str == NULL)
 		return (0);
-	while (s[i] != '\0')
+	while (i < len)
 	{
-		if (i >= start && j < len)
-		{
-			str[j] = s[i];
-			j++;
-		}
+		str[i] = s[start + i];
 		i++;
 	}
-	str[j] = '\0';
+	str[i] = '\0';
 	return (str);
 }
